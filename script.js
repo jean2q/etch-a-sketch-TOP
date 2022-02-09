18a2177b437d6549fd3b549be35736057c228a46
Jean Lima <pdelima.jeanlucas@gmail.com>
2022-02-09 20:41:35 -0300

    checkpoint changes

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ const clearBtn = document.querySelector('#clearButton');
 clearBtn.addEventListener("click", clearGrid)
 let gridSize = document.querySelector("#size").value
 createMyDivs(gridSize);
+let color = document.querySelector("#color").value
+console.log(color)
+
+determineWhichRadio();
+
+function determineWhichRadio(){
+    let allRadioButtons = document.querySelectorAll('input[type=radio]')
+    console.log(allRadioButtons);
+
+    allRadioButtons.forEach(element => {
+            element.addEventListener("click", determineColor);
+    });
+}
+
+function determineColor(e){
+    if (this.value == "solid"){
+        color = document.querySelector("#color").value
+        
+    }
+    else if(this.value == "randomColor"){
+        color = "#"+Math.floor(Math.random()*16777215).toString(16);
+        console.log(color)
+    }
+    else if(this.value == "darken"){
+        
+    }
+    console.log(e.target)
+}
 
 function createMyDivs(number){
     let wrapper = document.createElement("div");
@@ -28,8 +56,10 @@ function createMyDivs(number){
     .setProperty('--wrapperSize', wrapperSize);
 }
 
-function changeColor(){
-    this.setAttribute("style", "background-color:blue;")
+function changeColor(e){
+    e.target.setAttribute("style", "background-color:"+color+";")
+    console.log(e)
+    console.log(this)
 }
 
 function clearGrid(){
